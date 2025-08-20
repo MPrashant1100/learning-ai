1e4b6a1818be31ed7441ca21623417b4f589bf9c
mprashant1100 <prashantmishra1100@gmail.com>
2025-08-20 21:55:26 +0530

    day 7 step up and topics

---
+++ project06/main.py
@@ -0,0 +1,6 @@
+# Day 7: Text Summarization using LLMs
+# #	Topic
+# 1️⃣	What is Text Summarization? Types (Extractive vs Abstractive)
+# 2️⃣	Prompt Patterns for Summarization (Role, Format, Constraints)
+# 3️⃣	Build: Summarize a Blog Post using Gemini or OpenAI
+# 4️⃣	Bonus: Chunking Large Texts with Python 🧠
