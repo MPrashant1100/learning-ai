02e2c23286a9008708f18acfb71b98cdcc2d37fc
mprashant1100 <prashantmishra1100@gmail.com>
2025-08-21 23:23:25 +0530

    .

--- project06/main.py
+++ project06/main.py
@@ -3,4 +3,4 @@
 # 1️⃣	What is Text Summarization? Types (Extractive vs Abstractive)
 # 2️⃣	Prompt Patterns for Summarization (Role, Format, Constraints)
 # 3️⃣	Build: Summarize a Blog Post using Gemini or OpenAI
-# 4️⃣	Bonus: Chunking Large Texts with Python 🧠
+# 4️⃣	Bonus: Chunking Large Texts with Python 
